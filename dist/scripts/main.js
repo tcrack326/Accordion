93815f1519d49c2d5ec96dca512926650f1f1e3e
Thomas McCracken <thomas.alan.mccracken@gmail.com>
2014-10-06 10:44:49 -0400

    final

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,31 @@
-console.log('The Iron Yard Rocks');
+$(".closedBox").click(function() {
+$(".closedBox").removeClass("clickedBox");
+$(this).addClass("clickedBox");
+});
+
+/*
+$( "#sectionOne" ).click(function() {
+                          $("#sectionOne").removeClass("closedBox").addClass("clickedBox");
+
+$("#sectionTwo").removeClass("clickedBox").addClass("closedBox");
+
+$("#sectionThree").removeClass("clickedBox").addClass("closedBox");
+
+});
+
+$( "#sectionTwo" ).click(function() { $(this).removeClass("closedBox").addClass("clickedBox");
+
+$("#sectionOne").removeClass("clickedBox").addClass("closedBox");
+
+$("#sectionThree").removeClass("clickedBox").addClass("closedBox");
+
+});
+
+$( "#sectionThree" ).click(function() { $(this).removeClass("closedBox").addClass("clickedBox");
+
+$("#sectionOne").removeClass("clickedBox").addClass("closedBox");
+
+$("#sectionTwo").removeClass("clickedBox").addClass("closedBox");
+
+});
+*/
